f1d93cf7c7aa73ccdd8ac3f32568f413bd30dba0
Bruno Araujo <araujo.contato@gmail.com>
2018-10-18 20:02:37 -0300

    Form added to submit with enter key

--- app/scripts.js
+++ app/scripts.js
@@ -1,49 +1,64 @@
-const bar = document.querySelector('.yt-client-bar')
-    , remote = require('electron').remote
-    , WINDOW = remote.getCurrentWindow()
-let webview = document.querySelector('.yt-webview')
+const bar = document.querySelector('.yt-client-bar'),
+  remote = require('electron').remote,
+  WINDOW = remote.getCurrentWindow()
 
-const hideBar = ()=> {
+let webview = document.querySelector('.yt-webview'),
+  searchForm = document.querySelector('.search-bar'),
+  searchInput = document.querySelector('#search-input'),
+  searchSubmitButton = document.querySelector('.search-bar-item')
+
+const hideBar = () => {
   bar.style.height = '0'
   bar.style.padding = '0'
 }
 
-const showBar = ()=> {
+const showBar = () => {
   bar.style.height = '30px'
   bar.style.padding = '5px'
 }
 
-const pin = ()=> {
+const pin = () => {
   let icon = document.querySelector('.js-pin')
   console.log(icon)
   if (WINDOW.isAlwaysOnTop()) {
     icon.classList.remove('fa-lock')
     icon.classList.add('fa-unlock')
     WINDOW.setAlwaysOnTop(false)
-  }
-  else {
+  } else {
     icon.classList.remove('fa-unlock')
     icon.classList.add('fa-lock')
     WINDOW.setAlwaysOnTop(true)
   }
 }
 
-const minimizeWindow = ()=> {
+const minimizeWindow = () => {
   WINDOW.minimize()
 }
 
-const refresh = (url)=> {
-  webview.remove()
-  webview = document.createElement('WEBVIEW')
+const refresh = (url) => {
+  if (typeof url === 'undefined')
+    url = 'https://google.com'
 
   if (!url.includes('http'))
     url = 'https://' + url
 
+  searchInput.value = url
+
+  webview.remove()
+  webview = document.createElement('WEBVIEW')
   webview.src = url
   webview.classList.add('yt-webview')
   document.body.appendChild(webview)
 }
 
-const closeWindow = ()=> {
+const closeWindow = () => {
   WINDOW.close()
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  searchForm.addEventListener('submit', e => {
+    e.preventDefault()
+    searchSubmitButton.click()
+    return false
+  })
+})
